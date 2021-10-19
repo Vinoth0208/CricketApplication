f42bb268927ae6a53471e24407a4318b4c81f587
Karthik Sekar <karthiksekar011@gmail.com>
2021-10-19 23:28:08 +0530

    API exposed for teams and player

--- src/main/java/com/cricket/CricketApplication.java
+++ src/main/java/com/cricket/CricketApplication.java
@@ -1,17 +1,32 @@
 package com.cricket;
 
-import java.sql.*;
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CricketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CricketApplication.class, args);
-		
-	}	
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+//	    config.setAllowCredentials(true);
+	    // Don't do this in production, use a proper list  of allowed origins
+	    config.setAllowedOrigins(Collections.singletonList("*"));
+	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
 }
